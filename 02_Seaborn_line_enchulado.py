cf089cf588e47eb130472140d3de234467f92f23
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-09-15 11:25:24 -0300

    Add files via upload

---
+++ 02_Seaborn_line_enchulado.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Cargar los datos y preparar el DataFrame
+df = pd.read_csv(r'D:\Germán\Desktop\Python Files\automobile.csv')
+df = df.dropna(axis=0)
+
+# Personalizar el estilo de Seaborn
+sns.set(style="whitegrid")
+
+# Crear el gráfico de línea
+sns.lineplot(x='horsepower', y='price', data=df, ci=None, color='blue', label='Precio vs Potencia')
+
+# Personalizar el gráfico
+plt.title('Relación entre Potencia y Precio de Automóviles', fontsize=16)
+plt.xlabel('Potencia (Horsepower)', fontsize=12)
+plt.ylabel('Precio (Dólares)', fontsize=12)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+
+# Agregar una cuadrícula
+plt.grid(True, linestyle='--', alpha=0.6)
+
+# Agregar una leyenda
+plt.legend(loc='best', fontsize=12)
+
+# Mostrar el gráfico
+plt.show()
