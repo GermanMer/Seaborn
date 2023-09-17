0253a35e6c5e74df3dc54bb16a0483eebd03eda1
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-09-17 12:28:00 -0300

    Add files via upload

---
+++ 21_Seaborn_scatter.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Cargar los datos y preparar el DataFrame
+df = pd.read_csv(r'D:\Germán\Desktop\Python Files\automobile.csv')
+df = df.dropna(axis=0)
+
+# Genera el gráfico
+sns.scatterplot(x='horsepower', y='price', data=df)
+plt.show()
