1c9ba88f2941a1bfcac0fc80e1345ae280ef7e5a
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-09-17 12:27:34 -0300

    Add files via upload

---
+++ 20_Seaborn_box_XY_enchulado.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Cargar los datos y preparar el DataFrame
+df = pd.read_csv(r'D:\Germán\Desktop\Python Files\automobile.csv')
+df = df.dropna(axis=0)
+
+# Personalizar el estilo de Seaborn
+sns.set(style="whitegrid")
+
+# Crear el gráfico de caja
+sns.boxplot(x='horsepower', y='price', data=df, palette='Set3')  # Utiliza una paleta de colores personalizada
+
+# Personalizar el gráfico
+plt.title('Relación entre Potencia y Precio', fontsize=16)
+plt.xlabel('Potencia (Horsepower)', fontsize=12)
+plt.ylabel('Precio (Dólares)', fontsize=12)
+plt.xticks(rotation=45, fontsize=10)
+plt.yticks(fontsize=10)
+
+# Agregar líneas adicionales para resaltar valores atípicos (outliers)
+sns.stripplot(x='horsepower', y='price', data=df, color='gray', size=4, jitter=True)
+
+# Mostrar el gráfico
+plt.tight_layout()
+plt.show()
